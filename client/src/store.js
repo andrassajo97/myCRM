91df922a5f7087dcb9a5b0f24a0a75d76d836c64
Sajó András <43603768+andrassajo97@users.noreply.github.com>
2020-05-14 12:19:23 +0200

    store update

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const devTools = (process.env.NODE_ENV === 'production' ? null : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware), devTools
   )
 );
 
